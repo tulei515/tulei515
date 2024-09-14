3e6efdd21ddefc9ab59740bc2878f40b4cd3c511
tu.lei <tu.lei@ruanyun.net>
2024-09-14 15:03:04 +0800

    test

--- store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
-  roles: state => state.user.roles,
-  permissions: state => state.user.permissions,
-  dict: state => state.dict.dict,
-}
-export default getters
